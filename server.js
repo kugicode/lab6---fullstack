c224bda67cf5e76d7e3c223bdc810507108ec16c
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2025-11-01 01:10:32 +0000

    simple express webserver task

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const lessons = [
+    {id:1, name: 'Intro to html'},
+    {id:2, name: 'Intro to javasccript'},
+    {id:3, name: 'Intro to css'},
+    {id:4, name: 'Intro to vuejs'},
+    {id:5, name: 'Intro to react'}
+]
+
+const myLogger = (req, res, next) => {
+    console.log(`Incoming request ${req.url}`);
+    next();
+}
+
+app.use(myLogger);
+app.use('/images', express.static('img'));
+
+app.get('/' , (req, res) => {
+    res.send("Hello world");
+});
+
+app.get('/lessons', (req, res) => {
+    res.json(lessons);
+});
+
+app.get('/lessons/:id', (req, res) => {
+    //get requested id!
+    const id = parseInt(req.params.id);
+
+    const foundLesson = lessons.find(lesson => lesson.id === id);
+
+        if(foundLesson){
+            res.json(foundLesson)
+        }
+        else{
+            res.status(404).send("No lessons found!");
+        }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
