ac3c307e3f39b9ed255d5c0c77df7b7a746663e1
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2025-11-07 23:49:10 +0000

    added diffrent api routes

--- lessonsRouter.js
+++ lessonsRouter.js
@@ -2,14 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use(express.json());
+
+let nextId = 7;
 
 const lessons = [
-    {id: 1, name: 'Intro to html'},
-    {id: 2, name: 'Intro to javasccript'},
-    {id: 3, name: 'Intro to css'},
-    {id: 4, name: 'Intro to vuejs'},
-    {id: 5, name: 'Intro to react'},
-    {id: 6 , name: "Intro to Java"}
+    {id: 1, name: 'Intro to html', space: 5},
+    {id: 2, name: 'Intro to javasccript', space: 5},
+    {id: 3, name: 'Intro to css', space: 5},
+    {id: 4, name: 'Intro to vuejs', space: 5},
+    {id: 5, name: 'Intro to react', space: 5},
+    {id: 6 , name: "Intro to Java", space: 5}
 ];
 // Note: this router is mounted at /api/lessons in server.js
 router.get('/' , (req, res) => {
@@ -36,6 +39,39 @@ router.get('/:id', (req, res, next) => {
         res.json(foundLesson);
     } else {
         // pass to next middleware (likely the 404 handler in server)
+        next();
+    }
+});
+
+router.post('/', (req, res) => {
+    const newId = nextId++;
+
+    const newLesson = {
+        "id": newId,
+        "topic": "REST API", 
+        "location": "Brighton", 
+        "price": "300", 
+        "space": 100
+    }
+    lessons.push(newLesson);
+
+    res.json({id: newId});
+
+});
+
+// PUT /api/lessons/:id (Element 6: Update lesson space)
+router.put('/:id', (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+
+    const foundLesson = lessons.find(lesson => lesson.id === id);
+
+    if(foundLesson){
+        const currentSpace = foundLesson.space || 1;
+        foundLesson.space = currentSpace * 2;
+
+         res.json({"msg": "success"});
+    }
+    else{
         next();
     }
 });
